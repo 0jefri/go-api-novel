0713608d8c840e27de046c3976591d22e8cf976f
jefri agistar <jefri4876@gmail.com>
2024-09-03 02:08:03 +0700

    gagag

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "go-novel-api/delivery"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "SUKSESS BROOO")
+}
 
 func main() {
-	delivery.Server().Run()
-
+	http.HandleFunc("/", handler)
+	port := os.Getenv("PORT") // Render membutuhkan penggunaan PORT dari env var
+	if port == "" {
+		port = "8080" // Port default jika tidak ada env var
+	}
+	http.ListenAndServe(":"+port, nil)
 }
