d349033daba0197a0fc148779bf12ea8e5ceaaad
jefri agistar <jefri4876@gmail.com>
2024-09-03 02:15:50 +0700

    ffft

--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"os"
-)
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "SUKSESS BROOO")
-}
+import "go-novel-api/delivery"
 
 func main() {
-	http.HandleFunc("/", handler)
-	port := os.Getenv("PORT") // Render membutuhkan penggunaan PORT dari env var
-	if port == "" {
-		port = "8080" // Port default jika tidak ada env var
-	}
-	http.ListenAndServe(":"+port, nil)
+	delivery.Server().Run()
 }
